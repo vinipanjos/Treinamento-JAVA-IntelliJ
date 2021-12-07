a4df2ae59732747a9863455bdeb0b3e3fd3f6ec2
Vinícius <vinipanjos@hotmail.com>
2021-12-07 13:24:44 -0300

    Passando arquivos do JAVA para o IntelliJ

---
+++ src/Variaveis/Aula10Variaveis.java
@@ -0,0 +1,13 @@
+package Variaveis;
+
+public class Aula10Variaveis {
+    public static void main(String[] args) {
+        int idade = 27;
+        String nome = "Vinicius";
+        String nomeDoMeuCachorro = "Zip";
+        String ano2014 = "2014";
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome:" + nome);
+        String b = "Vinicius";
+    }
+}
