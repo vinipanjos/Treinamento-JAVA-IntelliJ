5b678890666d98c3c3bedd850a4be20400f2d172
vinipanjos <vinipanjos@hotmail.com>
2022-01-09 13:28:07 -0300

    Exercicio de formas geometricas com herança - Orientação a objetos - JAVA

---
+++ src/Aula44A46/Exercicios/Ex01/DimensaoSuperficial.java
@@ -0,0 +1,7 @@
+package Aula44A46.Exercicios.Ex01;
+
+public interface DimensaoSuperficial {
+
+    double calcularArea();
+
+}
---
+++ src/Aula44A46/Exercicios/Ex01/DimensaoVolumetrica.java
@@ -0,0 +1,7 @@
+package Aula44A46.Exercicios.Ex01;
+
+public interface DimensaoVolumetrica {
+
+    double calcularVolume();
+
+}
